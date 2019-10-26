e9ac8a5bd0a954c66c3f546e574b4a9022edab48
4h0y <ahoy@tutanota.de>
2019-10-27 01:12:19 +0300

    tld

--- yo.js
+++ yo.js
@@ -224,7 +224,7 @@ function yo(sel) {
     i.setAttribute('height', h);
     yohoho.setAttribute('style', style);
 
-    httpGetAsync('https://ahoy.yohoho.cc', p,
+    httpGetAsync('https://ahoy.yohoho.online', p,
         function (players) {
             var first = true;
             var buttons = document.createElement('div');
